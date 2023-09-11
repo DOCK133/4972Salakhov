b464c2f4dd00b54a34ea9ab85f84622203104a7d
dock133 <dock133@yandex.ru>
2023-09-11 22:08:14 +0300

    solution from sem1 task 2,4,6,8

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -3,4 +3,17 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число 1:");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите число 2:");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+if (num1 > num2)
+{
+    Console.WriteLine("Большее число " + num1 + " Меньшее число " + num2);
+}
+else
+{
+    Console.WriteLine("Большее число " + num2 + " Меньшее число " + num1);
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число 1:");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите число 2:");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите число 3:");
+
+int num3 = int.Parse(Console.ReadLine() ?? "0");
+
+if (num1 >= num2)
+{
+    if (num1 >= num3)
+
+        Console.Write("Большее число " + num1);
+}
+
+else
+
+   if (num2 >= num3)
+{
+    Console.Write("Большее число " + num2);
+}
+else
+{
+    Console.Write("Большее число " + num3);
+}
