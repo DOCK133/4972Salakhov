9b3165587b41d3bc8f87a0e16fd6529318621aaf
dock133 <dock133@yandex.ru>
2023-09-14 00:40:55 +0300

    Sem2Task

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число, и удаляет вторую цифру этого числа.
+
+void Variant1()
+{
+    int num = new Random().Next(100, 100);
+    Console.WriteLine(num);
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    num = firstDigit * 10 + lastDigit;
+    Console.WriteLine(num);
+}
+
+void Variant2()
+{
+    int ran = new System.Random().Next(100, 1000);
+    System.Console.WriteLine(ran);
+    char[] digitChar = ran.ToString().ToCharArray();
+    System.Console.Write(digitChar[0]);
+    System.Console.Write(digitChar[2]);
+}
+
+
+Variant1();
+Variant2();
---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,31 @@
+﻿void Variant1()
+{
+    Console.WriteLine("Введите число1: ");
+
+    int numA = int.Parse(Console.ReadLine() ?? "0");
+
+    Console.WriteLine("Введите число2: ");
+
+    int numB = int.Parse(Console.ReadLine() ?? "0");
+    if (numB % numA == 0)
+    {
+        Console.WriteLine("второе число кратно первому");
+    }
+    else
+    {
+        Console.WriteLine("второе число не кратно первому, остаток от деления " + numB % numA);
+    }
+}
+
+void Variant2()
+{
+    int a = int.Parse(Console.ReadLine() ?? "0");
+    int b = int.Parse(Console.ReadLine() ?? "0");
+
+    Console.WriteLine((a % b == 0) ? true : a % b);
+}
+
+
+
+Variant1();
+Variant2();
