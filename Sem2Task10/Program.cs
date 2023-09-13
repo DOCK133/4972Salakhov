8d2509fba10bbb1af5c3186ebf680d60cf5eb70f
dock133 <dock133@yandex.ru>
2023-09-14 00:49:13 +0300

    добавил дз 10

--- Sem2Task10/Program.cs
+++ Sem2Task10/Program.cs
@@ -2,3 +2,8 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+    int ran = new System.Random().Next(100, 1000);
+    System.Console.WriteLine(ran);
+    char[] digitChar = ran.ToString().ToCharArray();
+    System.Console.Write(digitChar[1]);
