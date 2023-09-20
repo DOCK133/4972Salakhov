9d30551ae1bdb26186fcf204850b0a9949dafddc
dock133 <dock133@yandex.ru>
2023-09-20 22:51:08 +0300

    Solution Promt Sem3

--- Sem3Task17/Program.cs
+++ Sem3Task17/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿// ЗАДАЧА 17
+// Напишите программу, которая принимает на вход координаты точки ( X и Y ), причем X = 0 и Y = 0 и 
+// выдает номер плоскости четверти, в которой находится эта точка.
+
+// 1) Ввести X
+// 2) Ввести Y
+// 3) Поиск ответа
+// 4) Вывести результат
+
+// Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// По координатам точки определяем номер четверти
+int QuterTest(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+int x = ReadData("Введите координату X: ");
+int y = ReadData("Введите координату Y: ");
+
+int res = QuterTest(x, y);
+
+PrintResult("Точка находится в четверти № " + res);
---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,28 @@
+﻿// ЗАДАНИЕ 18
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапозон возможных координат точек вэтой четверти (X и Y)
+
+// 1) Ввести номер четверти
+// 2) Ответить на вопрос
+// 3) Вывести ответ
+
+
+// Вводим данные
+int ReadData()
+{
+    Console.WriteLine("Введите номер четверти: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string QuterBorderAsk(int numQuter)
+{
+    if (numQuter == 1) return "x>0, y>0";
+    if (numQuter == 2) return "x>0, y<0";
+    if (numQuter == 3) return "x<0, y<0";
+    if (numQuter == 4) return "x<0, y>0";
+    return "Неверное значение";
+}
+
+// Печатаем результат
+int numQuter = ReadData();
+Console.WriteLine(QuterBorderAsk(numQuter));
