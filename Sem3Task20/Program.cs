b3501588928b753246bf040eb4bcdaa51cb87e86
dock133 <dock133@yandex.ru>
2023-09-20 22:53:28 +0300

    20

--- Sem3Task20/Program.cs
+++ Sem3Task20/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿//     ЗАДАЧА №20
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит растояние между ними в 2D пространстве.
+
+// 1) ReadData
+// 2) Calc2DDist
+// 3) PrintResult
+
+
+int X1 = ReadData("Введите координту X1: ");
+int X2 = ReadData("Введите координту X2: ");
+int Y1 = ReadData("Введите координту Y1: ");
+int Y2 = ReadData("Введите координту Y2: ");
+
+
+// double findDistance(int X1, int X2, int Y1, int Y2)
+// {
+//     return Math.Sqrt(Math.Pow (X1 - X2, 2) + Math.Pow (Y1 - Y2, 2));
+// }
+
+// int X1 = int.Parse(Console.ReadLine() ?? "0");
+// int X2 = int.Parse(Console.ReadLine() ?? "0");
+// int Y1 = int.Parse(Console.ReadLine() ?? "0");
+// int Y2 = int.Parse(Console.ReadLine() ?? "0");
+
+// Console.WriteLine(findDistance(X1, X2, Y1, Y2));
