773dd34cb4e2cf6ac5ff66b5b8baf2e27b3ffe0b
dock133 <dock133@yandex.ru>
2023-09-29 01:59:43 +0300

    Sem4Task29 done

--- Sem4Task29/Program.cs
+++ Sem4Task29/Program.cs
@@ -1,7 +1,37 @@
 ﻿// №29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // (задание со *) Ввести с клавиатуры длину массива и диапазон значений элементов
-// (задание со *) Дополнительно: Написать программу которая из имен через запятую выберет случайное имя и выведет в терминал
-// Игорь, Антон, Сергей -> Антон
-// Подсказка: Для разбора строки использовать метод string.split(). Для выбора случайного имени метод 
-// Random.Next(1,<длина массива имен>+1).
 
+// Вводим данные (определяем длинну массива)
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+// Генерируем массив случайными цифрами
+int[] GenArr(int gen)
+{
+    int[] arr = new int[gen];
+    Random rnd = new Random();
+    for (int i = 0; i < gen; i++)
+    {
+        arr[i] = rnd.Next(1,99);
+    }
+    return arr;
+}
+
+// Печатаем массив
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int gen = ReadData("Введите размер массива: ");
+//int[] arr = new int [gen];
+int[] arr = GenArr(gen);
+PrintArr(arr);
