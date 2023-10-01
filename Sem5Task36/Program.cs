ecde6587e6ff65103f2e9b5355779a0a4cd22295
dock133 <dock133@yandex.ru>
2023-10-01 19:05:46 +0300

    Sem5Task36

---
+++ Sem5Task36/Program.cs
@@ -0,0 +1,55 @@
+﻿// №36 Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [3, 7, -2, 1] -> 8
+// [-4, -6, 89, 6] -> 0
+// (со звездочкой) Найдите все пары в массиве и выведите пользователю
+
+// Генерируем массив случайными цифрами
+int[] GenArr(int num, int lowBorder, int highBorder)
+{
+    int[] arr = new int[num];
+    Random rnd = new Random();
+    for (int i = 0; i < num; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+    return arr;
+}
+
+// Печатаем массив
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Считаем числа в массиве на нечётных позициях
+int NegPosSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+
+// Вызываем генерацию массива 
+int[] arr = GenArr(8, -10, 20);
+// Выводим изначальный массив
+PrintArr(arr);
+// Считаем количество чисел
+int sum = NegPosSum(arr);
+// Печатаем результат
+PrintResult("Сумма элементов на нечётных позициях " + sum);
