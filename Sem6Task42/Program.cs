fd23144f67f5830fe31d240048d5147fbc05343e
dock133 <dock133@yandex.ru>
2023-10-04 20:31:33 +0300

    Sem6Task42-44

--- Sem6Task42/Program.cs
+++ Sem6Task42/Program.cs
@@ -4,3 +4,34 @@
 // 3   --> 11
 // 2   --> 10
 
+// Вывод сообщения и запись введённых данных
+int ReadData(string mes)
+{
+    Console.Write(mes);
+    string value = Console.ReadLine() ?? ",";
+    int num = Convert.ToInt32(value);
+
+    return num;
+}
+
+// Преобразовываем десятичное число в двоичное (простой нерекурентный способ)
+string DecToBin(int num)
+{
+    string line = "";
+    while (num > 0)
+    {
+        line = num % 2 + line;
+        num = num / 2;
+    }
+    return line;
+}
+
+// Переводит число в двоичное (рекурентный способ)
+string DecToBinRec(int number)
+{
+    return number == 0 ? "" : DecToBin(number / 2) + Convert.ToString(number % 2);
+}
+
+int num = ReadData("введите число ");
+Console.WriteLine("Двоичное представление числа(Simpl): " + DecToBin(num));
+Console.WriteLine("Двоичное представление числа(recurent): " + DecToBinRec(num));
---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 44:
+// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 --> 0 1 1 2 3
+// Если N = 3 --> 0 1 1
+// Если N = 7 --> 0 1 1 2 3 5 8
+
+// Вывод сообщения и запись введённых данных
+int ReadData(string mes)
+{
+    Console.Write(mes);
+    string value = Console.ReadLine() ?? ",";
+    int num = Convert.ToInt32(value);
+
+    return num;
+}
+
+void PrintFibNum(int num)
+{
+    int buf = 0;
+    int first = 0;
+    int second = 1;
+    Console.Write(first + " " + second);
+    for (int i = 2; i < num; i++)
+    {
+        Console.Write(" " + (first + second));
+        buf = first + second;
+        first = second;
+        second = buf;
+    }
+}
+
+int num = ReadData("введите число ");
+PrintFibNum(num);
