ac769512c35bbc7edaf95dec462cfd9adf27130c
dock133 <dock133@yandex.ru>
2023-10-09 00:21:45 +0300

    Sem 8 Task 54-56-58-60-62 ДЗ

--- Sem8Task54/Program.cs
+++ Sem8Task54/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного
+//  массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+// Функция заполнения массива рандомно числами
+void FillArrayRandom(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Функция сортировки элементов в строке двумерного массива, по убыванию
+void SortToMin(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Вывод элементы массива в консоль
+void Print2DArray(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] table = new int[3, 4];
+FillArrayRandom(table);
+Print2DArray(table);
+SortToMin(table);
+Console.WriteLine();
+Print2DArray(table);
---
+++ Sem8Task58/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// Вывод сообщения и запись введённых данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Метод заполнения массива рандомными числами от 1 до 9
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Вывод элементы массива в консоль
+void Print2DArray(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = ReadData("Введите количество строк: ");
+int columns = ReadData("Введите количество столбцов: ");
+int[,] array = new int[row, columns];
+int[,] secondArr = new int[row, columns];
+int[,] resultArr = new int[row, columns];
+
+FillArrayRandom(array);
+Print2DArray(array);
+
+Console.WriteLine();
+
+FillArrayRandom(secondArr);
+Print2DArray(secondArr);
+
+Console.WriteLine();
+
+if (array.GetLength(0) != secondArr.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArr.GetLength(1); j++)
+    {
+        resultArr[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArr[i, j] += array[i, k] * secondArr[k, j];
+        }
+    }
+}
+
+Print2DArray(resultArr);
